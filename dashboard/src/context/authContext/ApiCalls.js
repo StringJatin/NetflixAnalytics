cffe6ba3c0985300c13068b76e1bdd2149589ff3
Jatin Chouhan <jatinchouhan1515@gmail.com>
2024-01-20 16:50:59 +0530

    updated link in context api

--- dashboard/src/context/authContext/ApiCalls.js
+++ dashboard/src/context/authContext/ApiCalls.js
@@ -4,7 +4,7 @@ import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 export const loginapi = async (user, dispatch) => {
   dispatch(loginStart());
   try {
-    const res = await axios.post("http://localhost:3000/api/auth/login", user);
+    const res = await axios.post("https://netflix-analytics-4u5n.vercel.app/api/auth/login", user);
     res.data.isAdmin && dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
